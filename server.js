25da010329668cee96008e35a6ab396b11e28c68
MAC <mac@Kwames-MacBook-Pro.local>
2023-08-12 16:56:21 +0000

    Updated code: added declaring routes

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+//Declaring Routes - to run this app on my browser
+app.get('/', (req, res) => {
+    res.send('Restful CRUD API')
+})
+
+
+app.listen(3000, () => {
+    console.log('Node API app is running on port 3000')
+})
+
