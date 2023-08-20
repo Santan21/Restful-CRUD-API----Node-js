42d7afea15d383b6618c78aed330d99020562dbf
Santan21 <lesliegyamfi02@gmail.com>
2023-08-20 16:25:53 +0000

    connected MongoDB and added product model

---
+++ models/productmodel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const productSchema = moongoose.Schema (
+    {
+      name: {
+        type: String,
+        required: [true, "please enter a product name"]
+      } ,
+      quantity: {
+          type: Number,
+          required: 0
+      },
+      price: {
+        type: Number,
+        required: false
+      },
+      image: {
+        type: String,
+        required:false
+      }
+    },
+    {
+        timestamps: true
+    }
+)
--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 
 //Declaring Routes - to run this app on my browser
 
 app.get('/', (req, res) => {
-    res.send('My Node API')
+    res.send('Node API')
 })
 
-
-app.listen(3000, () => {
-    console.log('Node API app is running on port 3000')
+app.get('/blog', (req, res) => {
+    res.send('Blog API - this is leslie here')
 })
 
+mongoose.set("strictQuery", false)
+mongoose.connect(
+    'mongodb+srv://admin:Realasvp44@leslieapi.scqttyb.mongodb.net/Leslie-API?retryWrites=true&w=majority')
+    .then(() => {
+        console.log('Connected to MongoDB')
+        app.listen(3000, () => {
+            console.log('Node API app is running on port 3000')
+        });
+    }).catch((error) => {
+        console.log('error')
+    })
