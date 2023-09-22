f9968118bdab0f8baf07325bf8358cd6a908d359
Santan21 <lesliegyamfi02@gmail.com>
2023-09-22 13:35:14 +0000

    Fetched Data from MongoDB

--- server.js
+++ server.js
@@ -17,7 +17,16 @@ app.get('/blog', (req, res) => {
     res.send('Blog API - this is leslie here')
 })
 
-app.post('/product', async(req, res) => {
+app.get('/products', async(req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.mmessage}) 
+    }
+})
+
+app.post('/products', async(req, res) => {
     try {
        const product = await Product.create(req.body)
        res.status(200).json(product);
